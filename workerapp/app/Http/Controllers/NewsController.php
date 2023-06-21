4b007713d4ec868a5abd10d036b08765938778aa
tishev5667 <tishev5667@bk.ru>
2023-06-21 13:11:48 +0300

    5
    5

--- workerapp/app/Http/Controllers/NewsController.php
+++ workerapp/app/Http/Controllers/NewsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\News;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 
@@ -10,9 +12,10 @@ use Illuminate\Http\Request;
 class NewsController extends Controller
 {
 
-    public function index() 
+    public function index(): View
     {
-        $news = $this->getNews();
+        $model = app(News::class);
+        $news = $model->getNews();
         return view('news.index', [
             'newsList' => $news
         ]);
