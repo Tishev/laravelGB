6fd7f5cb699bf67daa8c854b4dc4d19a3c5d0bed
tishev5667 <tishev5667@bk.ru>
2023-07-15 14:46:39 +0300

    8
    8

--- workerapp/routes/web.php
+++ workerapp/routes/web.php
@@ -4,6 +4,8 @@
 use App\Http\Controllers\NewsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -30,6 +32,22 @@ Route::get('/news/{id}', [NewsController::class, 'show'])
 
 Route::match(["POST", "GET", "PUT"], '/test', function(Request $request) {
     return (int) $request->isMethod('GET');
+});
+
+
+Route::get('/collections', function () {
+    $collection = collect([1,2,3,4,5,77,8,9,34,86,64]);
+    dd($collection->toJson());
+});
+
+
+Route::get('/sessions', function () {
+    if (session()->has('mysession')) {
+        dd(session()->all(), session()->get('mysession'));
+        session()->forget('mysession');
+    }
+    // session()->put('mysession', 'Test Session');
+
 });
 
 
